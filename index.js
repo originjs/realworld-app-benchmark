e2ad084e562d3c0bef6212eb9841b22713c4c231
joey <flyfish.zy@gmail.com>
2024-01-23 18:07:30 +0800

    add lighthouse

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import * as fs from 'node:fs';
-import { execSync } from 'node:child_process';
+import { execSync, exec } from 'node:child_process';
+import * as chromeLauncher from "chrome-launcher";
+import lighthouse from "lighthouse";
 
 function buildProject(project) {
     console.log('Building project: ', project.name);
@@ -16,11 +18,57 @@ function runCommand(command, cwd) {
     execSync(command, { stdio: 'inherit', cwd });
 }
 
-function main() {
+async function runLighthouse(url) {
+    const debuggingPort = 9999;
+    const options = {
+        chromeFlags: [
+            "--headless",
+            "--no-sandbox",
+            "--no-first-run",
+            "--enable-automation",
+            "--disable-infobars",
+            "--disable-background-networking",
+            "--disable-background-timer-throttling",
+            "--disable-cache",
+            "--disable-translate",
+            "--disable-sync",
+            "--disable-extensions",
+            "--disable-default-apps",
+            "--window-size=1200,800",
+            "--remote-debugging-port=" + debuggingPort,
+        ],
+        onlyCategories: ["performance"],
+        port: debuggingPort,
+        logLevel: "info",
+    };
+    const chrome = await chromeLauncher.launch(options);
+    let results = null;
+    try {
+        results = await lighthouse(url, options);
+        await chrome.kill();
+    } catch (error) {
+        console.log("error running lighthouse", error);
+        await chrome.kill();
+        throw error;
+    }
+    return results.lhr;
+}
+
+async function main() {
+    process.stdout.setEncoding('utf-8');
     const projects = JSON.parse(fs.readFileSync('projectList.json'));
+    let port = 5000;
     for (const project of projects) {
-        console.log(project);
         buildProject(project);
+        const cmd = `"node_modules/.bin/serve" ./${project.path}/${project.dist ? project.dist : 'dist'} -l ${port}`;
+        console.log(cmd);
+        const serve = exec(cmd , (error) => {
+            throw error;
+        });
+        const result = await runLighthouse(`http://localhost:${port}/`);
+        fs.writeFileSync(`./results/${project.name}.json`, JSON.stringify(result));
+        serve.kill('SIGINT');
+        port++;
     }
 }
 
